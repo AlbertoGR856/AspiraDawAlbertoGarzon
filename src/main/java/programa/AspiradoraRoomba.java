c92c8d6c15759cf3e0509f8c1fca76bdd4afb3d4
Alberto <albertogr578@gmail.com>
2020-11-19 10:05:49 +0100

    Añade comentarios a las distintas partes del programa

--- src/main/java/programa/AspiradoraRoomba.java
+++ src/main/java/programa/AspiradoraRoomba.java
@@ -20,28 +20,32 @@ public class AspiradoraRoomba {
         Random random = new Random();
 
         //VARIABLES
-        //Usuario y contraseña
+        //Usuario y contraseña, variables de tipo String
         String usuario = "Alberto";
         String password = "Aspiradora";
 
-        //Habitaciones
+        //Habitaciones declaradas como variables de tipo entero e inicializadas a 0
         int cocinaInt = 0, salonInt = 0, cuartoBañoInt = 0, dormitorioUnoInt = 0, dormitorioDosInt = 0;
 
-        //CARGA
+        //Variable para establecer la carga de la bateria de tipo enttero e inicializada a 0
         int nivelBateria = 0;
 
         //String modoCompleto, modoCarga;
         //int estadoGeneral;
         //String baseCarga, salir;
+        //Una constante de tipo double para establecer el gasto de bateria en el modo aspiración y fregado
         final double aspiradoFregado = 2.25;
 
-        //Menu variables
+        //Variables de tipo entero para establecer las opciones del menu
         int opcionElegidaMenu;
         int opcionModo;
         int opcionElegidaMenuCaracteristicas;
+
+        //Variable de tipo booleano para establecer la salida del programa
         boolean repetir = true;
 
         //INTRODUCIR USUSARIO
+        //Bucle deo-while para repetir en caso de introducir mal el usuario y contraseña
         do {
 
             JOptionPane.showMessageDialog(null, "ASPIRADORA ROOMBA");
@@ -50,12 +54,15 @@ public class AspiradoraRoomba {
 
             password = JOptionPane.showInputDialog("Introduzca la contraseña: ");
 
+            //Si se introduce el usuario y la contraseña de forma correcta, aparece mensaje de confirmacion
+            // y se sale del bucle
             if (usuario.equals("Alberto") && password.equals("Aspiradora")) {
 
                 JOptionPane.showMessageDialog(null, "Bienvenido:\n Has ingresado "
                         + "satisfactoriamente al sistema", "Mensaje de bienvenida",
                         JOptionPane.INFORMATION_MESSAGE);
 
+                //En caso contrario se muestra mensaje de error y se volveria a pedir los datos
             } else {
 
                 JOptionPane.showMessageDialog(null, "Acceso denegado:\n"
@@ -64,10 +71,11 @@ public class AspiradoraRoomba {
 
                 JOptionPane.showMessageDialog(null, "El usuario es Alberto y la contraseña es Aspiradora");
             }
-
+            //Si se cumple la condición del while, se repite el bucle
         } while (!usuario.equals("Alberto") && !password.equals("Aspiradora"));
 
         //INTRODUCIR NUMERO DEPENDECIAS Y METROS
+        //Bucle do-while para cada dependencia donde los m2 no pueden pasar del valor 0-100
         do {
 
             String cocina = JOptionPane.showInputDialog("Introduzca los metros cuadrados de la cocina: ");
@@ -108,24 +116,31 @@ public class AspiradoraRoomba {
 
         } while (dormitorioDosInt < 0 || dormitorioDosInt > 100);
 
+        //Se muestra los metros de cada dependencia introducida
         JOptionPane.showMessageDialog(null, "Los metros cuadrados de la dependencias son: " + " cocina: " + cocinaInt + " m2" + " salón: " + salonInt + " m2" + " cuarto de baño: " + cuartoBañoInt + " m2"
                 + " dormitorio 1: " + dormitorioUnoInt + " m2" + " dormitorio 2: " + dormitorioDosInt + " m2");
 
         //INTRODUCIR NIVEL BATERIA
+        
+        //Bucle do-while donde el nivel de bateria no puede tener un valor fuera del rango 0-100
         do {
 
             String bateria = JOptionPane.showInputDialog(
                     null, "Introduzca el nivel de bateria (0-100");
 
             nivelBateria = Integer.parseInt(bateria);
-        } while (nivelBateria <= 0 || nivelBateria > 100);
+        } while (nivelBateria < 0 || nivelBateria > 100);
 
+        //Se muestra el nivel de bateria
         JOptionPane.showMessageDialog(
                 null, "El nivel de bateria es: " + nivelBateria + " %");
 
-        //MENU PRINCIPAL - CONFIGURAR SALIR
+        //MENU PRINCIPAL - CONFIGURAR Y SALIR
+        
+        //Con este do-while se repetira el bucle en caso de elegir no salir del programa
         do {
 
+            //Se tendria que elegir una opcion que no se salga de los parametros establecidos
             do {
 
                 String menu = JOptionPane.showInputDialog(null, "MENU ASPIRADORA \nEliga una opción:"
@@ -136,12 +151,13 @@ public class AspiradoraRoomba {
 
             } while (opcionElegidaMenu < 0 || opcionElegidaMenu > 1);
 
+            //Estrcutura Switch asociado cada case a una opcion del menú
             switch (opcionElegidaMenu) {
 
                 // CONFIGURAR SISTEMA
                 case 1:
 
-                    //Introducir nivel bateria
+                    
                     JOptionPane.showMessageDialog(
                             null, "Ha entrado usted configurar sistema");
 
@@ -159,7 +175,7 @@ public class AspiradoraRoomba {
                             JOptionPane.showMessageDialog(
                                     null, "Ha entrado usted en Modo completo");
 
-                            if (nivelBateria <= 0) {
+                            if (nivelBateria < 3) {
                                 //BASE CARGA
                                 JOptionPane.showMessageDialog(
                                         null, "Ha entrado usted en Base de carga: Entrando en modo de carga de bateria "
@@ -230,11 +246,12 @@ public class AspiradoraRoomba {
 
                         }
 
+                        //Si se cumple la condicion del while, se repite el bucle
                     } while (opcionModo < 0 || opcionModo > 3);
 
                     //MENU CARACTERISTICAS
                     do {
-
+                        //Se tendria que elegir una opcion que no se salga de los parametros establecidos
                         do {
 
                             String menuCaracteristicas = JOptionPane.showInputDialog(null, "MENU CARACTERISTICAS\nEliga una opción:\n1.- Fecha y Hora"
@@ -245,8 +262,11 @@ public class AspiradoraRoomba {
                             //Recoger una variable por consola
                             opcionElegidaMenuCaracteristicas = Integer.parseInt(menuCaracteristicas);
 
+                            //Se obitiene la hora, minutos y segundos actuales usando la clase LocalDateTime y instanciandola con
+                            // LocalDateTime.now
                             if (opcionElegidaMenuCaracteristicas == 1) {
 
+                                //Usando la funcion get. para tomar la hora, minutos y segundos
                                 LocalDateTime locaDate = LocalDateTime.now();
                                 int horas = locaDate.getHour();
                                 int minutos = locaDate.getMinute();
@@ -254,30 +274,41 @@ public class AspiradoraRoomba {
 
                                 JOptionPane.showMessageDialog(null, "Hora actual : " + horas + ":" + minutos + ":" + segundos);
 
+                                //Se muestra el niveld de batería
                             } else if (opcionElegidaMenuCaracteristicas == 2) {
 
                                 JOptionPane.showMessageDialog(
                                         null, "El nivel de bateria es: " + nivelBateria + " %");
 
+                                //Se muestra el lugar donde esta parado la aspiradora
                             } else if (opcionElegidaMenuCaracteristicas == 3) {
 
+                                JOptionPane.showMessageDialog(
+                                        null, "El aspirador se encuentra parado en: " + nivelBateria);
+
+                                //Se muestran las dependencias y sus metros cuadrados
                             } else if (opcionElegidaMenuCaracteristicas == 4) {
 
                                 JOptionPane.showMessageDialog(null, "Los metros cuadrados de la dependencias son: " + " cocina: " + cocinaInt + " m2" + " salón: " + salonInt + " m2" + " cuarto de baño: " + cuartoBañoInt + " m2"
                                         + " dormitorio 1: " + dormitorioUnoInt + " m2" + " dormitorio 2: " + dormitorioDosInt + " m2");
 
+                                //Opción para salir del menú
                             } else if (opcionElegidaMenuCaracteristicas == 5) {
 
                                 JOptionPane.showMessageDialog(null, "Saliendo del menu...");
 
                             }
 
+                            //Si se cumle la condicion del while, se repite el bucle
                         } while (opcionElegidaMenuCaracteristicas < 0 || opcionElegidaMenuCaracteristicas > 5);
 
+                         //Se pregunta al usuario si quiere salir del menú, con la ventana JOptionPane.YES_NO_OPTION
                         int op = JOptionPane.showConfirmDialog(null,
                                 "¿Deseas salir del menu?", "Salida del menú", JOptionPane.YES_NO_OPTION);
 
                         // System.out.println("Opcion: " + op); // Depuración
+                        
+                        //Estructura if-else donde el usuario decidira si salir o no del menú
                         if (op == JOptionPane.YES_OPTION) {
                             // Quiere salir
                             System.out.println("Saliendo del menú...."); // Depuración
@@ -288,9 +319,11 @@ public class AspiradoraRoomba {
                             System.out.println("Indique otra opcion:");
                         }
 
+                          //Si se cumple la condicion del while, el bucle se repetira, iniciando una nueva ejecución
                     } while (repetir);
 
                 //SALIDA
+                //Default de la estructura switch para salir del menú de modos de ejecución
                 default:
 
                     JOptionPane.showMessageDialog(
@@ -300,10 +333,13 @@ public class AspiradoraRoomba {
             }
 
             //PREGUNTAR PARA SALIR
+            //Se pregunta al usuario si quiere salir del programa, con la ventana JOptionPane.YES_NO_OPTION
             int op = JOptionPane.showConfirmDialog(null,
                     "¿Deseas salir?", "Salida del programa", JOptionPane.YES_NO_OPTION);
 
             // System.out.println("Opcion: " + op); // Depuración
+            
+            //Estructura if-else donde el usuario decidira si salir o no del programa
             if (op == JOptionPane.YES_OPTION) {
                 // Quiere salir
                 JOptionPane.showMessageDialog(
@@ -316,6 +352,7 @@ public class AspiradoraRoomba {
                         null, "Indique otra instrucción a realizar:");
             }
 
+            //Si se cumple la condicion del while, el bucle se repetira, iniciando una nueva ejecución
         } while (repetir);
 
     }
